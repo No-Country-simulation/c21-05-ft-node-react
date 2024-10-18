b1e762aa680ad8d99d1338a131473c8eb07c1135
Lautaro-R-collins <lautarorodriguezcollins@gmail.com>
2024-10-17 22:50:08 -0300

    commit

--- frontend/src/components/NewsCarousel.jsx
+++ frontend/src/components/NewsCarousel.jsx
@@ -1,12 +1,10 @@
 import React from 'react';
-import { Navigation, Pagination, Scrollbar, A11y } from 'swiper/modules';
+import { Navigation, Pagination, Scrollbar, A11y, Autoplay  } from 'swiper/modules';
 import { Swiper, SwiperSlide } from 'swiper/react';
-
 // Import Swiper styles
 import 'swiper/css';
-import 'swiper/css/bundle';
+import 'swiper/css/navigation'; 
 import 'swiper/css/pagination';
-import 'swiper/css/scrollbar';
 
 export default () => {
 
@@ -20,26 +18,30 @@ export default () => {
       {
         image: 'https://http2.mlstatic.com/storage/splinter-admin/o:f_webp,q_auto:best/1728058670104-desk-mainslider-landing-1.png',
       },
-      // Agregá más elementos 
+      // Agregar más elementos 
     ];
   
     return (
       <Swiper
-        modules={[Navigation, Pagination, Scrollbar, A11y]}
+        modules={[Navigation, Pagination, Scrollbar, A11y, Autoplay]}
         spaceBetween={30}
         slidesPerView={1}
         navigation
         pagination={{ clickable: true }}
         scrollbar={{ draggable: true }}
         loop={true}
+        autoplay ={{
+            delay: 3000,
+            disableOnInteraction: false,
+          }}
         >
         {items.map((item, index) => (
           <SwiperSlide key={index}>
             <div className="">
               <img
-                src={item.image}
+                src={item.image }
                 alt={item.title}
-                className="w-full h-96  object-cover"
+                className="w-full h-96 object-cover"
               />
             </div>
           </SwiperSlide>
--- frontend/src/components/header.jsx
+++ frontend/src/components/header.jsx
@@ -6,17 +6,17 @@ import { BuildingStorefrontIcon, ShoppingCartIcon, UserIcon } from '@heroicons/r
 
 const Header = () => {
   return (
-    <header className="flex items-center justify-between p-4 ">
+    <header className="flex items-center justify-between p-4 max-w-7xl mx-auto">
       <img src={logo} alt="Logo" className="w-36" />
       <nav className="flex space-x-4">
         <Link to="/store">
-          <BuildingStorefrontIcon className="h-6 w-6 hover:text-yellow-500 cursor-pointer" />
+          <BuildingStorefrontIcon className="h-6 w-6 hover:text-yellow-600 cursor-pointer" />
         </Link>
         <Link to="/cart">
-          <ShoppingCartIcon className="h-6 w-6 hover:text-yellow-500 cursor-pointer" />
+          <ShoppingCartIcon className="h-6 w-6 hover:text-yellow-600 cursor-pointer" />
         </Link>
         <Link to="/profile">
-          <UserIcon className="h-6 w-6 hover:text-yellow-500 cursor-pointer" />
+          <UserIcon className="h-6 w-6 hover:text-yellow-600 cursor-pointer" />
         </Link>
       </nav>
     </header>
