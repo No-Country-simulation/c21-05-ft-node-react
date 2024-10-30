1220ee2358dd85ebf956763889ceb8e9e88a727c
Camila Sironi <sironicamila98@gmail.com>
2024-10-30 13:39:50 -0300

    agregar ProductDetail page y Product component

--- frontend/src/pages/product-detail.jsx
+++ frontend/src/pages/product-detail.jsx
@@ -1,1 +1,19 @@
-// TO-DO
+import { useParams } from 'react-router-dom'
+import Layout from '../components/layout'
+import Product from '../components/product'
+
+const ProductDetail = () => {
+
+  const {id} = useParams();
+
+  return (
+    <Layout>
+      <main className="p-5 sm:p-10 min-h-screen">
+        <Product id={id}/>
+      </main>
+    </Layout>
+  );
+}
+
+export default ProductDetail
+
